ca570fc063c4e6ee9ff80ed81b5e7d50c8f51571
Vadim Latypov <latypov.vd@parmalogica.ru>
2024-11-03 20:51:10 +0500

    init

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+
+module.export = {
+    printWidth: 110,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    trailingComma: 'none',
+    arrowParens: always,
+    endOfLine: 'lf',
+}
